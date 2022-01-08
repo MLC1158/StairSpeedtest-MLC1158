df9ec1d0184fd5c083cf60d374c382ec74a2d21d
MLC1158 <60974273+MLC1158@users.noreply.github.com>
2022-01-09 02:50:18 +0800

    Update webgui.bat

--- base/webgui.bat
+++ base/webgui.bat
@@ -2,5 +2,5 @@
 chcp 65001>nul
 title Stair Speedtest Web GUI Backend
 setlocal enabledelayedexpansion
-start http://127.0.0.1:65430
+start http://127.0.0.1:6543
 tools\gui\websocketd --port=65430 --maxforks=100 --staticdir=tools\gui stairspeedtest /rpc
